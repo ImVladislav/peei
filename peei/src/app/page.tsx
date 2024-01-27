3488677ed08685ce8ea9e05e68c6fb2c648ab735
ImVladislav <nafanya102@gmail.com>
2024-01-27 21:50:19 +0200

    1

--- peei/src/app/page.tsx
+++ peei/src/app/page.tsx
@@ -1,12 +1,11 @@
-
 import AboutUs from "./aboutUs/page";
 import Activity from "./activity/page";
 
 export default function Home() {
   return (
     <>
-      <AboutUs />
-      <Activity/>
+      {/* <AboutUs />
+      <Activity/> */}
     </>
   );
 }
