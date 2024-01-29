35693a2f08cbc781e055a3c7a617941b7ce807f8
ImVladislav <nafanya102@gmail.com>
2024-01-29 21:28:33 +0200

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+hUKDOpmBZ8x0uxnhNGGgYubvT7hfnQB1PDb2ARI5c0=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"SyRBAMDdcb3eJSQvT6lIfXZtVzz3CPiFhfDlpocacVM=\"}"
